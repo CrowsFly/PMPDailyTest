fb5b03de4ee6fbf6bd9af9364f4eff0d34aa0491
1.txt <594969947@qq.com>
2019-05-10 08:44:23 +0800

    5.10日考

---
+++ 5.10/Math.php
@@ -0,0 +1,15 @@
+<?php
+
+	
+		
+			$s = 'Have you ever gone shopping and';
+			$len = strlen($s);
+			// var_dump($len);
+			// $arr = [];
+			// for($i;$i<$len;$i++){
+			// 	$a = $arr[$]
+			// }
+			echo 'e';
+	
+
+?>
